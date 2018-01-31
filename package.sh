8c243a9fc5e714f52a7b947e9fd81add08d6d05b
Jenkins <jenkins@brightedge.com>
2018-01-31 09:39:24 -0800

    Point to stable PHP SDK

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..
-VERSION_TAG=0.3.0
+VERSION_TAG=0.3.1
 tar cvfz be_ixf_drupal-8.x-$VERSION_TAG.tar.gz --exclude='.git' --exclude='vendor/*' --exclude='package.sh' be_ixf_drupal
 cd -
 
