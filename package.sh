2be8b21b188af66c034e6012496063892e9ed93a
Sammy Yu <syu@brightedge.com>
2018-01-31 12:01:26 -0800

    Updated version tag in package

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..
-VERSION_TAG=0.3.1
+VERSION_TAG=1.0-beta3
 tar cvfz be_ixf_drupal-8.x-$VERSION_TAG.tar.gz --exclude='.git' --exclude='vendor/*' --exclude='package.sh' be_ixf_drupal
 cd -
 
